b996f7ef278132b363bf1531e8a18dc6ef5b5555
Carlos Caceres <ccaceres461@gmail.com>
2025-02-22 16:21:38 -0400

    Add control and content chats

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import styles from "./App.module.css";
+import { Chat } from "./components/Chat/Chat";
+import { Controls } from "./components/Chat/Controls/Controls";
+
 
 function App() {
+const [messages, setMessages] = useState(MESSAGES);
+
+
   return (
     <div className={styles.App} >
       <header className={styles.Header}>
@@ -8,10 +15,38 @@ function App() {
         <h2 className={styles.Title}>IA Chatbot</h2>
       </header>
       <div className={styles.ChatContainer} >
-
+        <Chat messages={messages} />
       </div>
+      <Controls />
     </div>
   )
 }
+
+const MESSAGES = [
+  {
+    role: 'user',
+    content:'Lorem test'
+  },
+  {
+    role: 'assistant',
+    content:'Lorem test2'
+  },
+  {
+    role: 'user',
+    content:'Lorem test'
+  },
+  {
+    role: 'assistant',
+    content:'Lorem test2'
+  },
+  {
+    role: 'user',
+    content:'Lorem test'
+  },
+  {
+    role: 'assistant',
+    content:'Lorem test2'
+  }
+];
 
 export default App
---
+++ src/components/Chat/Chat.jsx
@@ -0,0 +1,10 @@
+import styles from "./Chat.module.css"
+
+export function Chat({ messages }) {
+    return (
+        <div className={styles.Chat} >{ messages.map(({role, content}, index)=>(
+            <div className={styles.Message} key={index} data-role={role}  >
+               {content}</div>
+   )) }</div>
+    );
+}
